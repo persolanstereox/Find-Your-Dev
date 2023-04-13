4c93471692ddfc9fb545f498bdd07565771114f9
Piotr Jemielita <persolanstereox@gmail.com>
2023-04-13 20:06:49 +0200

    fetching part 83737

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Header from "./components/Header";
 import Container from "./components/UI/Container";
 import SearchBar from "./components/SearchBar";
@@ -6,21 +6,64 @@ import UserInfo from "./components/UserInfo";
 import UserStats from "./components/UserStats";
 import styles from "./app.module.css";
 import useFetch from "./custom hooks/useFetch";
-import {TOKEN, URL} from './config.js'
+import { TOKEN, URL } from "./config.js";
 
 function App() {
   // console.log(TOKEN)
- 
-  // console.log(data)
+
+  // // console.log(data)
+  // const [shouldFetch, setShouldFetch] = useState(false)
+  const [url, setUrl] = useState("");
   const [theme, setTheme] = useState("DARK");
-  const [username, setUsername] = useState('')
-  const [data, isLoading, error] = useFetch(URL);
+  const [username, setUsername] = useState("");
+  // const {data, isLoading, error, fetchData, refetchData} = useFetch();
+  // const  {fetchData} = useFetch();
 
+  // const getUserData = () => {
+  //   // console.log(username)
+  //   // console.log(URL)
+  //   // console.log(URL + username)
+  //   // setUrl(URL + username)
+  //   // setShouldFetch((curr) => (curr === false ? true : false))
 
-  const getUserData = () => {
-    const user = data
-    console.log(user)
+  //   // // console.log(data)
+  //   // // setUsername(username)
+  //   // // setShouldFetch((curr) => (curr === false ? true : false))
+  //   // // const user = data.id
+  //   // // console.log(user)
+  //   // fetchData(url)
+  //   // console.log(data)
+  // //   setUrl(URL + username)
+  // //   refetchData(url)
+  // //   console.log(data)
+
+  // }
+
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  async function fetchData() {
+    
+    setIsLoading(true);
+    try {
+      setUrl(URL + username);
+      
+      const response = await fetch(url);
+
+      if (!response.ok) {
+        throw new Error("Request failed");
+      }
+      const json = await response.json();
+      setData(json);
+      console.log(data);
+    } catch (error) {
+      setError(error.message || "Something went wrong!");
+    }
+    setIsLoading(false);
   }
+
+  // return { data, isLoading, error, fetchData, refetchData};
 
   const themeHandler = () => {
     setTheme((curr) => (curr === "DARK" ? "LIGHT" : "DARK"));
@@ -29,10 +72,14 @@ function App() {
     <Container>
       <main className={styles.app}>
         <Header theme={theme} themeHandler={themeHandler} />
-        <SearchBar getUser={getUserData} setUsername={setUsername}/>
+        <SearchBar getUser={fetchData} setUsername={setUsername} />
         <div className={styles.info_container}>
-        <UserInfo/>
-        <UserStats/>
+          {error && <h2>error</h2>}
+          {isLoading && <h2>Loading...</h2>}
+          {data && <UserInfo />}
+          {data && <UserStats/>}
+          {/* <UserInfo />
+          <UserStats /> */}
         </div>
       </main>
     </Container>
--- src/custom hooks/useFetch.js
+++ src/custom hooks/useFetch.js
@@ -1,30 +1,73 @@
 import { useState, useEffect } from "react";
 
-function useFetch(url) {
+// function useFetch(url, shouldFetch) {
+//   const [data, setData] = useState('');
+//   const [isLoading, setIsLoading] = useState(false);
+//   const [error, setError] = useState(null);
+
+//   useEffect(() => {
+
+//     if (shouldFetch) {
+//       async function fetchData() {
+//         setIsLoading(true);
+//         try {
+//           const response = await fetch(url);
+
+//           if (!response.ok) {
+//             throw new Error("Request failed");
+//           }
+//           const json = await response.json();
+//           setData(json);
+//         } catch (error) {
+//           setError(error.message || "Something went wrong!");
+//         }
+//         setIsLoading(false);
+//       }
+//       fetchData();
+//     }
+//   }, [shouldFetch]);
+
+//   return [data, isLoading, error];
+// }
+function useFetch() {
   const [data, setData] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  useEffect(() => {
-    async function fetchData() {
-      setIsLoading(true);
-      try {
-        const response = await fetch(url);
+  async function fetchData(url) {
+    setIsLoading(true);
+    try {
+      const response = await fetch(url);
 
-        if (!response.ok) {
-          throw new Error("Request failed");
-        }
-        const json = await response.json();
-        setData(json);
-      } catch (error) {
-        setError(error.message || "Something went wrong!");
+      if (!response.ok) {
+        throw new Error("Request failed");
       }
-      setIsLoading(false);
+      const json = await response.json();
+      setData(json);
+    } catch (error) {
+      setError(error.message || "Something went wrong!");
     }
-    fetchData();
-  }, [url]);
+    setIsLoading(false);
+  }
+  async function refetchData(url) {
+    setIsLoading(true);
+    try {
+      const response = await fetch(url);
 
-  return [data, isLoading, error];
+      if (!response.ok) {
+        throw new Error("Request failed");
+      }
+      const json = await response.json();
+      setData(json);
+      console.log(data)
+    } catch (error) {
+      setError(error.message || "Something went wrong!");
+    }
+    setIsLoading(false);
+  }
+  
+
+  return { data, isLoading, error, fetchData, refetchData};
 }
 
 export default useFetch;
