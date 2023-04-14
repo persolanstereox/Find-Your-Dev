3bdc3b0f6c45f8f60d3ed242811b44e0ffe20cd7
Piotr Jemielita <persolanstereox@gmail.com>
2023-04-14 21:51:39 +0200

    styling

--- src/App.jsx
+++ src/App.jsx
@@ -5,28 +5,47 @@ import Container from "./components/UI/Container";
 import SearchBar from "./components/SearchBar";
 import UserInfo from "./components/UserInfo";
 import UserStats from "./components/UserStats";
+import RunAppInfo from "./components/RunAppInfo";
 import styles from "./app.module.css";
 
 function App() {
-  
   const [url, setUrl] = useState("");
   const [theme, setTheme] = useState("DARK");
   const [data, setData] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
+  
+
   async function fetchData() {
-    setIsLoading(true)
+    setIsLoading(true);
     try {
-      
       const response = await axios.get(url);
       console.log(response.data);
-      ///test
+      const filteredData = {
+        avatar: response.data.avatar_url,
+        bio: response.data.bio,
+        name: response.data.name,
+        username: response.data.login,
+        github_url: response.data.html_url,
+        joined: response.data.created_at,
+        repos: response.data.public_repos,
+        followers: response.data.followers,
+        following: response.data.following,
+        location: response.data.location,
+        private_site: response.data.blog,
+        twitter: response.data.twitter_username,
+        company: response.data.company,
+      };
+      setData(filteredData);
+      setError(null)
+      console.log(filteredData);
     } catch (error) {
       setError(error.message || "Something went wrong!");
+      setData(null)
       console.error(error);
     }
-    setIsLoading(false)
+    setIsLoading(false);
   }
 
   const themeHandler = () => {
@@ -40,9 +59,9 @@ function App() {
         <div className={styles.info_container}>
           {error && <h2>error</h2>}
           {isLoading && <h2>Loading...</h2>}
-          {data && <UserInfo />}
-          {data && <UserStats/>}
-          
+          {!data && <RunAppInfo/>}
+          {data && <UserInfo userData={data} />}
+          {data && <UserStats userData={data} />}
         </div>
       </main>
     </Container>
--- src/components/UserStats.jsx
+++ src/components/UserStats.jsx
@@ -2,23 +2,25 @@ import InfoBadge from "../components/UI/InfoBadge";
 import linkIcon from "/link.png";
 import mapMarkerIcon from "/map.png";
 import twitterIcon from "/twitter.png";
+import companyIcon from "/company-icon.png";
 import styles from "../assets/css/userstats.module.css";
 
 const UserStats = (props) => {
+  const userdata = props.userData;
   return (
     <section className={styles.user_stats_container}>
       <div className={styles.stats_box}>
         <div>
           <span>Repos</span>
-          <h2>8</h2>
+          <h2>{userdata.repos}</h2>
         </div>
         <div>
           <span>Followers</span>
-          <h2>2115</h2>
+          <h2>{userdata.followers}</h2>
         </div>
         <div>
           <span>Following</span>
-          <h2>9</h2>
+          <h2>{userdata.following}</h2>
         </div>
       </div>
       <div className={styles.additional_info_box}>
@@ -26,23 +28,23 @@ const UserStats = (props) => {
           <InfoBadge
             styles={styles.info_and_icon_box}
             imgSrc={mapMarkerIcon}
-            span={"San Francisco"}
+            span={userdata.location || "Not available"}
           />
           <div className={styles.info_and_icon_box}>
             <img src={linkIcon} alt="Link Icon" />
-            <a href="#">https:///gmgmmgmg.com</a>
+            <a href={userdata.privateSite || '#'}>{userdata.private_site || "Not available"} </a>
           </div>
         </div>
         <div>
           <InfoBadge
             styles={styles.info_and_icon_box}
             imgSrc={twitterIcon}
-            span={"Not available"}
+            span={userdata.twitter || "Not available"}
           />
           <InfoBadge
             styles={styles.info_and_icon_box}
-            imgSrc={twitterIcon}
-            span={"@github"}
+            imgSrc={companyIcon}
+            span={userdata.company || "Not available"}
           />
         </div>
       </div>
